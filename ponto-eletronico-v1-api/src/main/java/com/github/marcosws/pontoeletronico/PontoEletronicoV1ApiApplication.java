899dece2b94cb17c76fd431ebcd671a80fdbdd15
Marcos <marcosws@ymail.com>
2023-08-25 11:56:04 -0300

    Inicio projeto ponto-eletronico-v1-api

---
+++ ponto-eletronico-v1-api/src/main/java/com/github/marcosws/pontoeletronico/PontoEletronicoV1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcosws.pontoeletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletronicoV1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoEletronicoV1ApiApplication.class, args);
+	}
+
+}
---
+++ ponto-eletronico-v1-api/src/test/java/com/github/marcosws/pontoeletronico/PontoEletronicoV1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcosws.pontoeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoEletronicoV1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
